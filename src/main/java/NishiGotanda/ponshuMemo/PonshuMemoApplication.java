03a3bd6adb1292f09b8172d8f2d00a7693c2ba8a
TAIKI KIDA <taikikida@TAIKInoMacBook-Pro.local>
2022-10-29 12:30:08 +0900

    最初のコミット

---
+++ src/main/java/NishiGotanda/ponshuMemo/PonshuMemoApplication.java
@@ -0,0 +1,13 @@
+package NishiGotanda.ponshuMemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonshuMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonshuMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NishiGotanda/ponshuMemo/PonshuMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package NishiGotanda.ponshuMemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonshuMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
